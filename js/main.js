59e5ebf8c2624954c3f48d0f91c65a6d915d7db0
Bikrom-Roy <bikromroy2001@gmail.com>
2022-09-02 23:05:19 +0600

    category data load

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('hello')
+const loadData = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLoadData(data.data.news_category))
+}
+
+const displayLoadData = categorys => {
+    const categoryContainer = document.getElementById('Category-container');
+    categorys.forEach(category => {
+        console.log(category);
+        const licategory = document.createElement('li');
+        licategory.classList.add('nav-item');
+        licategory.innerHTML = `
+        
+        <a onclick="loadAllCategory" class="nav-link active" aria-current="page" href="#">${category.category_name}</a>
+        `;
+        categoryContainer.appendChild(licategory);
+    })
+}
+
+loadData();
